fdac1f83e7571db106c235d5a1afc2404cbb5b7f
Jakub Serafin <sirthaven@gmail.com>
2018-02-26 22:50:00 +0100

    persisting nickname

--- ClientApp/components/ChatTextbox.tsx
+++ ClientApp/components/ChatTextbox.tsx
@@ -3,22 +3,37 @@ import { RouteComponentProps } from "react-router";
 
 interface ChatTextState{
     message: string
+    nick: string
+    nickEntered: boolean;
 }
 
 export class ChatTextbox extends React.Component<{}, ChatTextState> {
     constructor()
     {
         super();
-        this.state = {message:""}
+        //this.props= props;
+        this.state = {message:"", nick:"", nickEntered:false}
+        
     }
 
+    private getNicknamePanel()
+    {
+            if(!this.state.nickEntered)
+            {
+                return <input type="text" className="form-control " id="usr" value={this.state.nick} 
+                    onChange={(msg)=> this.setState({nick:msg.target.value})}
+                    onKeyDown={key=>{if(key.key== "Enter" && this.state.nick.length>1) this.setState({nickEntered:true})}}></input>                                
+            }
+            return <span className="user-name">{this.state.nick}: </span>
+    }
     public render()
     {
         return <div className="form-horizontal">
-            <div className="form-group form-group-sm">
-            <div className="col-xs-8 col-sm-4 col-md-4">
-                <input type="text" className="form-control " id="usr" value={this.state.message} onChange={(msg)=> this.setState({message:msg.target.value})} ></input>
-            </div>
+            <div className="form-group form-group-sm form-inline">
+            <label>
+                {this.getNicknamePanel()}
+                <input type="text" className="form-control " id="msg" value={this.state.message} onKeyDown={key=>{if(key.key == "Enter")this.SendMessage()}} onChange={(msg)=> this.setState({message:msg.target.value})} ></input>
+            </label>   
                 <button type="button" className="btn" onClick={()=>this.SendMessage()}>Send</button>
             </div>
         </div>
